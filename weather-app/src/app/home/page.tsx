4eb63c5cfe22345a5bcd6550a8265522f8fbdd41
CodeGuru15 <arijitpal6111@gmail.com>
2024-04-09 00:08:37 +0530

    Nextjs setup done

---
+++ weather-app/src/app/home/page.tsx
@@ -0,0 +1,11 @@
+const MyHome = () => {
+  return (
+    <>
+      <h1 className=" text-center text-2xl text-blue-600 font-bold">
+        Happy Hacking
+      </h1>
+    </>
+  );
+};
+
+export default MyHome;
